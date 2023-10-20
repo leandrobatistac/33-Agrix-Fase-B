5eb2a07773e12555a494427b2eef3a64888b73c7
leandrobatista <leandrobcrv@gmail.com>
2023-10-20 00:14:17 -0300

    Iniciando o Projeto

--- src/main/java/com/betrybe/agrix/AgrixApplication.java
+++ src/main/java/com/betrybe/agrix/AgrixApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Application main class.
+ * Application  main class.
  */
 @SpringBootApplication
 public class AgrixApplication {
